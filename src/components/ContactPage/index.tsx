18b1a857266d5e96d4439d9328c9d8a455e4ca54
Saahil <neon@saahild.com>
2024-06-29 00:09:46 -0400

    Update Contact form to not be BEHIND bg

--- src/components/ContactPage/index.tsx
+++ src/components/ContactPage/index.tsx
@@ -38,12 +38,12 @@ if(Math.random() > .5) {
         
     }
     const ui = (f: any) => (e: any) => f(e.target.value)
-    return <div className="hero min-h-screen ">
-    <div className="text-center">
-      <div className="max-w-md">
-        <h1 className="text-5xl font-bold text-highlight mt-10">Contact Form</h1>
+    return <div className="min-h-screen hero " style={{ zIndex: 99999999 }}>
+    <div className="text-center" style={{ zIndex: 99999999 }}>
+      <div className="max-w-md" style={{ zIndex: 99999999 }}>
+        <h1 className="mt-10 text-5xl font-bold text-highlight" >Contact Form</h1>
         {/* <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> */}
-{(error || done) ? <FormResults error={error} email={email} done={done} /> : <ContactForm onSubmit={onSubmit} 
+{(error || done) ? <FormResults error={error} email={email} done={done}  /> : <ContactForm onSubmit={onSubmit} 
 email={email} uiE={ui(setEmail)} 
 message={message} uiM={ui(setMessage)}
 nameOfPerson={nameOfPerson} uiN={ui(setName)}
@@ -71,27 +71,27 @@ export function ContactForm({ onSubmit,
      nameOfPerson, message, email,
     uiE,uiN,uiM 
 }: any) {
-    return <form onSubmit={onSubmit} className="mt-5">
-<label className="input input-bordered flex items-center mb-2">
+    return <form onSubmit={onSubmit} className="mt-5" style={{ zIndex: 9999 }}>
+<label className="flex items-center mb-2 input input-bordered">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="var(--mauve)" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-  <input required type="email" className="grow" placeholder="Email" value={email} onChange={uiE} />
+  <input style={{ zIndex: 99999 }} required type="email" className="grow" placeholder="Email" value={email} onChange={uiE} />
 </label>
-<label className="input input-bordered flex items-center mb-2">
+<label className="flex items-center mb-2 input input-bordered">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="var(--mauve)" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-  <input required type="text" className="grow" placeholder="Name" value={nameOfPerson} onChange={uiN} />
+  <input style={{ zIndex: 99999 }} required type="text" className="grow" placeholder="Name" value={nameOfPerson} onChange={uiN} />
 </label>
 <label className="form-control">
   <div className="label">
     <span className="label-text">Message</span>
   </div>
-  <textarea required value={message} onChange={uiM} className="textarea textarea-bordered h-24" placeholder="Hi there neon..." style={{ borderColor: "var(--mauve)"}}></textarea>
+  <textarea required value={message} onChange={uiM} className="h-24 textarea textarea-bordered" placeholder="Hi there neon..." style={{ borderColor: "var(--mauve)", zIndex: 99999 }}></textarea>
   <div className="label">
     {/* <span className="label-text-alt">Your bio</span>
     <span className="label-text-alt">Alt label</span> */}
   </div>
 </label>
-<div>
-    <button type="submit" className="btn mauve" style={{ background: "var(--surface0)"}}>Submit</button>
+<div style={{ zIndex: 99999, overflow: "hidden" }}>
+    <button type="submit" className="btn mauve" style={{ background: "var(--surface0)", zIndex: 9999999 }}>Submit</button>
 </div>
     <br />
     </form>
