e945704c4691da4a7e764b531095512702e7d8db
Saahil <neon@saahild.com>
2024-06-28 23:44:15 -0400

    Fix Mobile UI for AboutPage

--- src/components/AboutPage/index.tsx
+++ src/components/AboutPage/index.tsx
@@ -2,16 +2,16 @@ import { mdiBullet } from '@mdi/js'
 import Icon from '@mdi/react'
 
 export default function AboutPage () {
-  return <div className={'hero min-h-screen'}>
+  return <div className={'hero min-h-screen md:ml-0 ml-2'}>
     <div className='container'>
-      <h1 className='font-bold text-3xl'>
+      <h1 className='text-3xl font-bold'>
 Some info about <span className='text-highlight'>me</span>
       </h1>
       <div>
-        <blockquote className='blockquote mb-0'>
+        <blockquote className='mb-0 blockquote'>
           <p style={{ textAlign: 'justify' }}>
             Hi Everyone, I am{' '}
-            <span className='text-highlight  '>Saahil dutta, </span>
+            <span className='text-highlight '>Saahil dutta, </span>
             from <span className='text-highlight '> USA </span>
             <br />I am a 9th grader in KCD.
             <br />
@@ -34,10 +34,10 @@ Some info about <span className='text-highlight'>me</span>
           </ul>
 
           <blockquote className='ml-10 text-xl italic font-semibold text-gray-900 dark:text-white' style={{ color: 'var(--text)'}}>
-            <svg className='w-8 h-8 text-gray-400 dark:text-gray-600 mb-4' xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 18 14'>
+            <svg className='w-8 h-8 mb-4 text-gray-400 dark:text-gray-600' xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 18 14'>
               <path d='M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z' />
             </svg>
-            <p className='ml-5 hover:text-fuchsia-600 ease-linear duration-700 text-fuchsia-500 first-letter:font-2xl'>
+            <p className='ml-5 duration-700 ease-linear hover:text-fuchsia-600 text-fuchsia-500 first-letter:font-2xl'>
             "It has become appallingly obvious that our technology has exceeded
             our humanity."{' '}
             </p>
