42f643f5ffee9e453f83ad29cc7a72ce7c2189c1
Saahil <neon@saahild.com>
2024-12-21 23:41:07 -0500

    feat: init iframe (#13)
    Co-authored-by: zeon-neon[bot] <136533918+zeon-neon[bot]@users.noreply.github.com>

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -86,6 +86,15 @@ export default function Footer() {
           <p>
             Designed by <span className="text-highlight">Saahil</span>
           </p>
+          <iframe
+            src="https://webring.phthallo.com/api/widgets/1?format=image&style=margin-right:10px"
+            width={360}
+            height={80}
+            style={{
+              paddingTop: "15px",
+              marginRight: "-35px",
+            }}
+          />
           <a href="https://ctp-webr.ing/neon/previous" className="font-bold">
             &larr;
           </a>
