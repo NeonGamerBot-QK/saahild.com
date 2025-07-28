4aab739f322a94a5aa2199b69c68214ddc59fccc
Saahil <neon@saahild.com>
2025-07-28 17:53:23 -0400

    done: projects zeon:ai_comments

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,1 +1,10 @@
-<template>projects who?</template>
+<!-- zeon, https://github.com/NeonGamerBot-QK/brown-washer, https://github.com/NeonGamerBot-QK/signal-app -->
+<template>
+<div>
+    <h1 class="font-bold text-2xl text-center mt-5">My projects</h1>
+<div class="">
+    <ProjectGrid />
+</div>
+</div>
+</template>
+ 
